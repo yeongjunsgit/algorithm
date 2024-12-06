ca0b08661f64461fc3605a0f84c1b6734aa89974
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2024-12-07 03:25:16 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long N, M;
+    cin >> N >> M;
+    
+    long long result = abs(N - M);
+    
+    cout << result;
+    
+}
