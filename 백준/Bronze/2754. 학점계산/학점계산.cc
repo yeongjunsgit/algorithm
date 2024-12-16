f3bdcff4bd0e7b5fe744b93ed2e42864e865fcf1
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2024-12-16 16:26:17 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<string, string> score = {{"A+", "4.3"}, {"A0", "4.0"}, {"A-", "3.7"}, {"B+", "3.3"}, {"B0", "3.0"}, {"B-", "2.7"},
+{"C+", "2.3"}, {"C0", "2.0"}, {"C-", "1.7"}, {"D+", "1.3"}, {"D0", "1.0"}, {"D-", "0.7"}, {"F", "0.0"} };
+
+int main()
+{
+    string data;
+    cin >> data;
+    cout << score[data];
+
+    return 0;
+}
