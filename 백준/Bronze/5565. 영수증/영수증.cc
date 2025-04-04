8139bdb5fd90cae0fbd6341067db9269d3f413e8
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2025-04-05 01:36:36 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    
+    cin >> N;
+    
+    for (int i=0; i<9; ++i) {
+        cin >> M;
+        N -= M;
+    }
+    
+    cout << N;
+
+    return 0;
+}
