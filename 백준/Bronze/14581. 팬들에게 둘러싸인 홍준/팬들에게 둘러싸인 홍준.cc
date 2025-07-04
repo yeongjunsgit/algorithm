be7501aba09bd9788750285719cf2a31df3101f1
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2025-07-05 00:23:11 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    
+    cout << ":fan::fan::fan:\n:fan::" << name << "::fan:\n:fan::fan::fan:";
+
+    return 0;
+}
