72173fa66ce87fb7b9c79dc36b025dc3d6e0b05c
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2025-08-12 01:45:15 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one, two, three;
+    cin >> one >> two;
+    three = two + (two - one);
+    
+    cout << three;
+
+    return 0;
+}
