4d3d598402932be31965d5ee6da4e8777da1b626
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2025-09-30 02:20:30 +0900

    [Bronze IV] Title: 새천년관, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string naming = "";
+    naming += "n";
+    naming += "ku";
+    naming += "gwan";
+    
+    cout << naming;
+
+    return 0;
+}
