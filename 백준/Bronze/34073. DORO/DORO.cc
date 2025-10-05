1ebd2e01d4c7b9c91b272cfc534b51aa0963aaf3
dudwns020202 <139421121+yeongjunsgit@users.noreply.github.com>
2025-10-05 22:16:35 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    string data;
+    for (int i=0; i<N; ++i) {
+        cin >> data;
+        cout << data << "DORO" << " ";
+    }
+    return 0;
+}
